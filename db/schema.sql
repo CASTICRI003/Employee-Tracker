2202995cc96e24420298cf08feef60bbca7191f1
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-12 12:44:26 -0600

    set up role and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,28 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT forkey_role
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    CONSTRAINT forkey_manager
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
