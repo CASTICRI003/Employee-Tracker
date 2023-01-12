e289e6ad11e876952d52b35f5731c335e1d3f883
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-12 13:02:13 -0600

    added db connect

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
 const db = require('./config/connection');
+
+
+db.connect(err => {
+    if (err) throw err;
+    console.log(`Server runnning on 3001`);
+    intro();
+});
+
